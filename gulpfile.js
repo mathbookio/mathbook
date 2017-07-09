df6311f0ea41795d1122e4a278b401dbdfe296f1
Johny Georges <jgeorges371@gmail.com>
2017-07-09 11:51:43 -0400

    added broswersync

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@
 
 const gulp = require('gulp')
 const plugins = require('gulp-load-plugins')()
+const browserSync = require('browser-sync').create()
 
 const files = {
   allFiles: ['./src/**/*.js', './tests/*.spec.js'],
   testFiles: ['./tests/*.spec.js'],
-  srcFiles: ['./src/**/*.js']
+  srcFiles: ['./src/**/*.js', './src/**/*.pug', './src/**/*.css']
 }
 
 gulp.task('test:config', () => {
@@ -35,6 +36,47 @@ gulp.task('lint', function () {
       breakOnError: true,
       quiet: true
     }))
+})
+
+// we'd need a slight delay to reload browsers
+// connected to browser-sync after restarting nodemon
+const BROWSER_SYNC_RELOAD_DELAY = 500
+
+gulp.task('nodemon', function (cb) {
+  let called = false
+  return plugins.nodemon({
+
+    // nodemon our expressjs server
+    script: './bin/www'
+  })
+    .on('start', function onStart () {
+      // ensure start only got called once
+      if (!called) { cb() }
+      called = true
+    })
+    .on('restart', function onRestart () {
+      // reload connected browsers after a slight delay
+      setTimeout(function reload () {
+        browserSync.reload({
+          stream: false
+        })
+      }, BROWSER_SYNC_RELOAD_DELAY)
+    })
+})
+
+gulp.task('serve', ['nodemon'], function () {
+  // for more browser-sync config options: http://www.browsersync.io/docs/options/
+  browserSync.init({
+
+    // informs browser-sync to proxy our expressjs app which would run at the following location
+    proxy: 'http://localhost:3000',
+
+    // informs browser-sync to use the following port for the proxied app
+    // notice that the default port is 3000, which would clash with our expressjs
+    port: 4000
+  })
+
+  gulp.watch(files.srcFiles).on('change', browserSync.reload)
 })
 
 gulp.task('default', () =>
