206b4685a767b5bd05f7d5882fb8dea54759873c
Johny Georges <jgeorges371@gmail.com>
2017-11-27 17:41:16 -0500

    added get tutorial data helper and applied it to preview and review api endpoints.

--- src/apis/getTutorialData.js
+++ src/apis/getTutorialData.js
@@ -1,58 +1,13 @@
 'use strict'
 const _ = require('lodash')
-const github = require('../github-client')
-const Base64 = require('js-base64').Base64
-const constants = require('../../config/constants.json')
-const branchPrefix = constants.BRANCH_PREFIX
-const basePath = constants.TUTORIALS_PATH
-const repoName = constants.REPO
+const getTutorialData = require('./dataHelpers/getTutorialData')
 module.exports = function (req, res) {
   // get authenticated user
   const branchName = _.get(req, 'params.branch')
-  const branch = `${branchPrefix}/${branchName}`
-  const configPath = `${basePath}/${branchName}/config.json`
-  const contentPath = `${basePath}/${branchName}/content.json`
-  const exercisesPath = `${basePath}/${branchName}/exercises.json`
-  return github.users.get({})
-  .then((result) => {
-    const login = result.data.login
-    return login
-  })
-  .then((username) => {
-    return github.repos.getContent({
-      owner: username,
-      repo: repoName,
-      path: configPath,
-      ref: branch
-    })
-    .then((configFile) => {
-      const configData = JSON.parse(Base64.decode(configFile.data.content))
-      console.dir({ configData }, {depth: 10})
-      return github.repos.getContent({
-        owner: username,
-        repo: repoName,
-        path: contentPath,
-        ref: branch
-      })
-      .then((contentFile) => {
-        const contentData = JSON.parse(Base64.decode(contentFile.data.content))
-        console.dir({ contentData }, {depth: 10})
-        return github.repos.getContent({
-          owner: username,
-          repo: repoName,
-          path: exercisesPath,
-          ref: branch
-        })
-        .then((exerciseFile) => {
-          const exerciseData = JSON.parse(Base64.decode(exerciseFile.data.content))
-          console.dir({ exerciseData }, {depth: 10})
-          res.send({ config: configData, content: contentData, exercises: exerciseData })
-        })
-      })
-    })
-  })
+  return getTutorialData(branchName)
+  .then((data) => res.send(data))
   .catch((err) => {
     console.log('error getting tutorial Data', err)
-    res.status(err.code).send(err)
+    res.status(500).send(err)
   })
 }
--- src/apis/reviewTutorial.js
+++ src/apis/reviewTutorial.js
@@ -1,86 +1,16 @@
 'use strict'
 
 const _ = require('lodash')
-const github = require('../github-client')
-const Base64 = require('js-base64').Base64
-const constants = require('../../config/constants.json')
-const branchPrefix = constants.BRANCH_PREFIX
-const repoOwner = constants.OWNER
-const repoName = constants.REPO
-const basePath = constants.TUTORIALS_PATH
+const getTutorialData = require('./dataHelpers/getTutorialData')
 
 module.exports = function (req, res) {
   const user = _.get(req, 'params.user', '')
   const tutorialName = _.get(req, 'params.tutorialName', '')
-  const branch = `${branchPrefix}/${tutorialName}`
-  const head = `${user}:${branchPrefix}/${tutorialName}`
-  const configPath = `${basePath}/${tutorialName}/config.json`
-  const contentPath = `${basePath}/${tutorialName}/content.json`
-  const exercisesPath = `${basePath}/${tutorialName}/exercises.json`
-  console.log({
-    head
+  return getTutorialData(tutorialName, user)
+  .then((data) => res.send(data))
+  .catch((err) => {
+    console.error('reviewTutorial::getTutorialData', err)
+    res.status(err.code).send(err)
   })
-  return github.pullRequests.getAll({
-      owner: repoOwner,
-      repo: repoName,
-      state: 'open',
-      head: head
-    })
-    .then((result) => {
-      const exists = _.get(result, 'data', []).length > 0
-
-      if (exists === false) {
-        res.status(404).send({ code: 'ResourceNotFound', message: 'No open PR is available for the requested user and tutorial.' })
-        return
-      }
-      console.dir({ result }, { depth: 100 })
-      return github.repos.getContent({
-          owner: user,
-          repo: repoName,
-          path: configPath,
-          ref: branch
-        })
-        .then((configFile) => {
-          const configData = JSON.parse(Base64.decode(configFile.data.content))
-          console.dir({
-            configData
-          }, {
-            depth: 10
-          })
-          return github.repos.getContent({
-              owner: user,
-              repo: repoName,
-              path: contentPath,
-              ref: branch
-            })
-            .then((contentFile) => {
-              const contentData = JSON.parse(Base64.decode(contentFile.data.content))
-              console.dir({
-                contentData
-              }, {
-                depth: 10
-              })
-              return github.repos.getContent({
-                  owner: user,
-                  repo: repoName,
-                  path: exercisesPath,
-                  ref: branch
-                })
-                .then((exerciseFile) => {
-                  const exerciseData = JSON.parse(Base64.decode(exerciseFile.data.content))
-                  console.dir({
-                    exerciseData
-                  }, {
-                    depth: 10
-                  })
-                  res.send({
-                    config: configData,
-                    content: contentData,
-                    exercises: exerciseData
-                  })
-                })
-            })
-        })
-    })
-
+  
 }
