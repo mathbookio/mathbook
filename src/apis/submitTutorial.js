f10d93da0b1e7ff4d32711b87c73946e965671e9
Johny Georges <jgeorges371@gmail.com>
2017-11-30 14:59:27 -0500

    refactored submitTutorial to have cleaner error handling

--- src/apis/submitTutorial.js
+++ src/apis/submitTutorial.js
@@ -1,4 +1,5 @@
 "use strict"
+const _ = require("lodash")
 const github = require("../github-client")
 const constants = require("../../config/constants.json")
 const branchPrefix = constants.BRANCH_PREFIX
@@ -6,41 +7,61 @@ const repoOwner = constants.OWNER
 const repoName = constants.REPO
 const baseBranch = constants.BASE_BRANCH
 
-module.exports = function(req, res) {
-  console.log("YOU HIT /v1/submit/tutorial")
-  const tutorialName = req.body.tutorialName
-  let submitDescription = req.body.submitDescription
-  // get authenticated user
-  return github.users
-    .get({})
-    .then(result => {
-      const login = result.data.login
-      console.log("login", login)
-      return login
+module.exports = async function(req, res) {
+  const tutorialName = _.get(req, "body.tutorialName", "")
+  let submitDescription = _.get(req, "body.submitDescription", "")
+
+  try {
+    // get authenticated user
+    const username = await getUsername()
+    const reviewUrl = `http://localhost:4000/review/${username}/${tutorialName}`
+    submitDescription += `\n\n Here is the link to preview the tutorial [${reviewUrl}](${reviewUrl})`
+    const head = `${username}:${branchPrefix}/${tutorialName}`
+    const submitTitle = `merge tutorial ${tutorialName} into ${baseBranch}`
+    const submitTutorial = await createPullRequest(submitTitle, submitDescription, head)
+    const pullRequestUrl = submitTutorial["html_url"]
+    res.send({
+      submitted: true,
+      pullRequestUrl: pullRequestUrl,
+      tutorial: tutorialName
     })
-    .then(username => {
-      const reviewUrl = `http://localhost:4000/review/${username}/${tutorialName}`
-      submitDescription += `\n\n Here is the link to preview the tutorial [${reviewUrl}](${reviewUrl})`
-      return github.pullRequests.create({
-        owner: repoOwner,
-        repo: repoName,
-        title: `merge tutorial ${tutorialName} into ${baseBranch}`,
-        body: submitDescription,
-        head: `${username}:${branchPrefix}/${tutorialName}`,
-        base: baseBranch
-      })
+  } catch (err) {
+    console.log(`there was error when trying to submit a tutorial: ${tutorialName}`, err)
+    let error
+    if (err.code === 422) {
+      error = {
+        status: 404,
+        code: "ResourceNotFound",
+        message: "Unable to submit tutorial because the tutorial could not be found."
+      }
+    } else {
+      error = {
+        status: 500,
+        code: "InternalServerError",
+        message: "Uh-oh something broke on the server side of things. Unable to submit tutorial."
+      }
+    }
+    res.status(error.status).send(error)
+  }
+}
+
+function getUsername() {
+  return github.users.get({}).then(result => {
+    const login = result.data.login
+    console.log("login", login)
+    return login
+  })
+}
+
+function createPullRequest(title, body, head) {
+  return github.pullRequests
+    .create({
+      owner: repoOwner,
+      repo: repoName,
+      title: title,
+      body: body,
+      head: head,
+      base: baseBranch
     })
-    .then(prResult => {
-      console.log({ prResult })
-      const pullRequestUrl = prResult.data["html_url"]
-      res.send({
-        submitted: true,
-        pullRequestUrl: pullRequestUrl,
-        tutorial: tutorialName
-      })
-    })
-    .catch(err => {
-      console.log("error submitting branches/tutorial", err)
-      res.status(400).send(err)
-    })
+    .then(prResult => prResult.data)
 }
