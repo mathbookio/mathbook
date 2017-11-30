8235ef08a93569c78b63795671713e9c8da64b9d
Johny Georges <jgeorges371@gmail.com>
2017-11-30 16:54:49 -0500

    renamed some stuff in getTutorialByUsername file.

--- src/apis/getTutorialByUsername.js
+++ src/apis/getTutorialByUsername.js
@@ -4,12 +4,12 @@ const _ = require("lodash")
 const getTutorialData = require("./dataHelpers/getTutorialData")
 
 module.exports = function(req, res) {
-  const user = _.get(req, "params.user", "")
+  const username = _.get(req, "params.user", "")
   const tutorialName = _.get(req, "params.tutorialName", "")
-  return getTutorialData(tutorialName, user)
+  return getTutorialData(tutorialName, username)
     .then(data => res.send(data))
     .catch(err => {
-      console.error("reviewTutorial::getTutorialData", err)
+      console.error("reviewTutorial::getTutorialData::catch::err", err)
       res.status(err.code).send(err)
     })
 }
