9fef70b04073c905b25381bad8ed407bcd685a68
Johny Georges <jgeorges371@gmail.com>
2017-11-30 12:56:17 -0500

    refactored deleteTutorial api to use async await and better error handling

--- src/apis/deleteTutorial.js
+++ src/apis/deleteTutorial.js
@@ -5,30 +5,45 @@ const constants = require("../../config/constants.json")
 const repoName = constants.REPO
 const branchPrefix = constants.BRANCH_PREFIX
 
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
   const tutorialName = _.get(req, "body.tutorialName", "")
   // get authenticated user
-  return github.users
-    .get({})
-    .then(result => {
-      const login = result.data.login
-      console.log("login", login)
-      return login
-    })
-    .then(username => {
-      const repo = "testing"
-      return github.gitdata.deleteReference({
-        owner: username,
-        repo: repoName,
-        ref: `heads/${branchPrefix}/${tutorialName}`
-      })
-    })
-    .then(deleteResult => {
-      console.log({ deleteResult })
-      res.send({ deleted: true, tutorial: tutorialName })
-    })
-    .catch(err => {
-      console.log("error deleting branches/tutorials", err)
-      res.status(500).send(err)
-    })
+  try {
+    const username = await getUsername()
+    const ref = `heads/${branchPrefix}/${tutorialName}`
+    await deleteBranch(username, ref)
+    res.sendStatus(201)
+  } catch (err) {
+    console.log("an error occured when trying to delete tutorial", err)
+    let error
+    if (err.code === 422) {
+      error = {
+        status: 404,
+        code: "ResourceNotFound",
+        message: "The tutorial that we attempted to delete could not be found"
+      }
+    } else {
+      error = {
+        status: 500,
+        code: "InternalServerError",
+        message: "Uh-oh something broke on the server side of things. Unable to delete tutorial."
+      }
+    }
+    res.status(error.status).send(error)
+  }
+}
+
+function getUsername() {
+  return github.users.get({}).then(result => {
+    const login = result.data.login
+    return login
+  })
+}
+
+function deleteBranch(username, ref) {
+  return github.gitdata.deleteReference({
+    owner: username,
+    repo: repoName,
+    ref: ref
+  })
 }
