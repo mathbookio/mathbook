31b25c1bb6eb9d8dc5555a426f803849ec99f8f6
Johny Georges <jgeorges371@gmail.com>
2017-12-10 17:41:30 -0500

    updated session timeout to 1 day and osi icon. Also set body height to 100% so that the body is always full-height.

--- src/server/github-oauth.js
+++ src/server/github-oauth.js
@@ -2,10 +2,12 @@
 const config = require("../../config/config")()
 const CLIENT_ID = config.get("github.clientId")
 const CLIENT_SECRET = config.get("github.clientSecret")
+const host = config.get("bin.host")
+const port = config.get("bin.port")
 const githubOAuth = require("github-oauth")({
   githubClient: CLIENT_ID,
   githubSecret: CLIENT_SECRET,
-  baseURL: "http://localhost:4000",
+  baseURL: `http://${host}:${port}`,
   loginURI: "/login/authenticate",
   callbackURI: "/login/success",
   scope: "user%20repo" // optional, default scope is set to user
